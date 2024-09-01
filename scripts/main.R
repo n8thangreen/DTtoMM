747bd46b96bec34671fe0458d80ba816a8a80cef
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-09-01 15:52:26 +0100

    ideas for structure

---
+++ scripts/main.R
@@ -0,0 +1,89 @@
+
+library(tibble)
+library(dplyr)
+
+decision_tree <- tibble(
+  decision = rep(c("Treatment A", "Treatment B"), each = 3),
+  outcome = c("Success", "Failure", "Adverse Event", "Success", "Failure", "Adverse Event"),
+  probability = c(0.7, 0.2, 0.1, 0.6, 0.3, 0.1),
+  cost = c(1000, 2000, 500, 800, 2500, 600),
+  effectiveness = c(0.9, 0.5, 0.1, 0.85, 0.4, 0.2)
+)
+
+# sefine S3 classs
+
+DecisionTree <- function(data) {
+  structure(list(data = data), class = c("DecisionTree", "Model"))
+}
+
+MarkovModel <- function(init_probs, trans_matrix = NA, n_cycles = 10) {
+  structure(list(init_probs = init_probs,
+                 trans_matrix = trans_matrix,
+                 n_cycles = n_cycles),
+            class = c("MarkovModel", "Model"))
+}
+
+CombinedModel <- function(...) {
+  args <- list(...)
+  
+  if (any(sapply(args, inherits) != "Model")) {
+    stop("All arguments must be of class 'Model'")
+  }
+  
+  structure(args, class = "CombinedModel")
+}
+
+run_model.DecisionTree <- function() {
+  tree %>%
+    group_by(decision) %>%
+    summarise(
+      expected_cost = sum(probability * cost),
+      expected_effectiveness = sum(probability * effectiveness)
+    )
+}
+
+run_model.MarkovModel <- function() {
+  
+}
+
+run_model.CombinedModel <- function(model) {
+  
+  for (i in 1:length(model)) {
+    model_results <- run_model(model[[i]])
+  }
+  
+  model_results
+}
+
+get_costs.DecisionTree <- function(model) {
+  results$expected_cost
+}
+
+#
+get_costs.MarkovModel <- function(model) {
+  results$cumulative_cost
+}
+
+#
+get_costs.CombinedModel <- function(model) {
+  total_cost <- 0
+  for (i in 1:length(model)) {
+    total_cost <- total_cost + get_costs(model)
+  }
+  total_cost
+}
+
+# adaptor
+
+map_terminal_to_markov <- function(probs, mapping) {
+
+}
+
+mm_adaptor <- function(x, ...) {
+  UseMethod("mm_adaptor")
+}
+
+mm_adaptor.DecisionTree <- function(x, mapping, ...) {
+  term_probs <- x$term_probs
+  init_probs <- map_terminal_to_markov(term_probs, mapping)
+}
